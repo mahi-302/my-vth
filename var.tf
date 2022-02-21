3c12cc3e54ec7a0c0cece1b11322d7e7b0593006
BODA MAHESH KUMAR <93734751+mahi-302@users.noreply.github.com>
2022-02-22 01:36:15 +0530

    Update and rename variable.tf to var.tf

--- var.tf
+++ var.tf
@@ -8,7 +8,7 @@ variable "function_name" {
 }
 variable "role" {
   type = string
-  default = "arn:aws:iam::204379001184:instance-profile/role_all"
+  default = "arn:aws:iam::204379001184:role/rolelbda"
 }
 variable "handler" {
   type = string
